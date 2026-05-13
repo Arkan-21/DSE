a1fa439a75b4d8dc0500bb58e206fc2d1454c660
kinglarry05 <weedalars@gmail.com>
2026-05-13 11:55:20 +0200

    thrust inter

--- empirical_drag.py
+++ empirical_drag.py
@@ -277,7 +277,7 @@ def print_sample_table(
 
 if __name__ == "__main__":
     # Tweak these for your vehicle / mission point.
-    altitude_m = 35_000.0
+    altitude_m = 25_000.0
     S_ref = 425.682
 
     alpha_values_deg = [-1.0, 0.0, 1.0, 2.0, 3.0, 3.5, 4.0, 5.0, 7.5, 10.0]
